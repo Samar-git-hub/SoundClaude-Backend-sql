a6aed09c3ee2c57a3bbab85e4799c98b02be9e51
Mokshjain131 <mokshjain131@gmail.com>
2025-04-06 11:32:45 +0530

    sonoteller api

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const qs = require('querystring');
+const http = require('https');
+
+const options = {
+	method: 'POST',
+	hostname: 'sonoteller-ai1.p.rapidapi.com',
+	port: null,
+	path: '/lyrics_ddex',
+	headers: {
+		'x-rapidapi-host': 'sonoteller-ai1.p.rapidapi.com',
+		'Content-Type': 'application/x-www-form-urlencoded'
+	}
+};
+
+const req = http.request(options, function (res) {
+	const chunks = [];
+
+	res.on('data', function (chunk) {
+		chunks.push(chunk);
+	});
+
+	res.on('end', function () {
+		const body = Buffer.concat(chunks);
+		console.log(body.toString());
+	});
+});
+
+req.write(qs.stringify({
+  file: 'https://storage.googleapis.com/musikame-files/thefatrat-mayday-feat-laura-brehm-lyriclyrics-videocopyright-free-music.mp3'
+}));
+req.end();
